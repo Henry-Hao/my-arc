256c7493cabbee4df90f4aea60ba3c5a052f81e6
Yixian Hao <haoyixian1994@hotmail.com>
2021-09-06 16:03:13 +0800

    1. Implemented Clone for MyArc<T>. 2. Implemented Drop for MyArc<T>.

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,13 @@ impl<T> MyArc<T> {
             _marker: PhantomData
         }
     }
+
+    pub fn count(&self) -> usize {
+        let inner = self.ptr.as_ptr();
+        unsafe {
+            (*inner).rc.load(atomic::Ordering::Acquire)
+        }
+    }
 }
 
 impl<T> Deref for MyArc<T> {
@@ -48,6 +55,50 @@ impl<T> DerefMut for MyArc<T> {
         &mut inner.data
     }
 }
+
+impl<T> Clone for MyArc<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.ptr.as_ref() };
+        // use Ordering::Relaxed because we don't need any synchronization.
+        let old_rc = inner.rc.fetch_add(1, atomic::Ordering::Relaxed);
+        // In the case that someone cloned MyArc then use std::mem::forget to forget it without
+        // running the destructor(decrease rc), the memory will be overflowed. So a threshold is
+        // necessary.
+        if old_rc >= isize::MAX as usize {
+            std::process::abort();
+        }
+
+        Self {
+            ptr: self.ptr,
+            _marker: PhantomData
+        }
+    }
+}
+
+/// 1. decrease rc if it is greater than 1
+/// 2. if rc equals to 1(only one reference remaining)
+///     - 1. set a barrier to prevernt reorder of use and deletion of the data
+///     - 2. drop inner data
+impl<T> Drop for MyArc<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { self.ptr.as_ref() };
+        if (*inner).rc.fetch_sub(1, atomic::Ordering::Release) != 1 {
+            return;
+        }
+        atomic::fence(atomic::Ordering::Acquire);
+
+        unsafe {
+            Box::from_raw(self.ptr.as_ptr());
+        }
+    }
+}
+
+impl<T> Drop for ArcInner<T> {
+    fn drop(&mut self) {
+
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::MyArc;
@@ -69,4 +120,23 @@ mod tests {
         *a = 3;
         assert_eq!(*a, 3);
     }
+
+    #[test]
+    fn test_clone() {
+        let a = MyArc::new(0);
+        assert_eq!(a.count(), 1);
+        let b = a.clone();
+        assert_eq!(a.count(), 2);
+    }
+
+    #[test]
+    fn test_drop() {
+        let a = MyArc::new(2);
+        assert_eq!(a.count(), 1);
+        let b = a.clone();
+        assert_eq!(a.count(), 2);
+        drop(a);
+        assert_eq!(b.count(), 1);
+    }
+
 }
